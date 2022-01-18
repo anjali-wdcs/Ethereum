057d563592cfc900f9c10e1320946c2143015a2e
Anjali Sharma <92917156+anjali-wdcs@users.noreply.github.com>
2022-01-18 14:53:19 +0545

    async

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const app = express();
-const websocket = require('ws');
+console.log('Before');
+getUser(1, (user) => {
+    getRepos(user.github, (repos) => {
+        console.log('Repos', repos);
+    });
+});
+console.log('After');
 
-const server = require('http').createServer(app);
+function getUser(id, callback){
+    setTimeout(() =>{
+        console.log('Reading a user from database...');
+        callback({id: id, github: 'Anjali'});
+    }, 3000);
+}
 
-app.get('/',(req,res) =>  {
-    console.log("listening")
-    var Web3 = require('web3');
-    var web3 = new Web3('https://speedy-nodes-nyc.moralis.io/23c5a393f65202cc699ac2a6/eth/ropsten');
-    var block = web3.eth.getBlockNumber().then(console.log);
-    res.send(block)
-
-})
-
-server.listen(3000, () => console.log(`Listening on port :3000`))
+function getRepos(username, callback){
+    setTimeout(() =>{
+        console.log('Calling github API...')
+        callback([ 'repo1', 'repo2', 'repo3']);
+    }, 3000);
+}
